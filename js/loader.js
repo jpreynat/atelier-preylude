d3828e30e004afe5e902d2f719f96c6fd5d54c38
Johan Preynat <johan.preynat@gmail.com>
2017-06-05 00:58:34 +0200

    Update global style with new logo

--- js/loader.js
+++ js/loader.js
@@ -2,24 +2,24 @@ $(document).ready(function () {
     // Get the container for main-content
     var container = $('#main-content'),
         initialPage = 'home',
-    
+
     // This object will cache the ajax loaded pages
         loadedPages = {},
         map,
-    
+
     // Google maps options
         mapOptions = {
             center: new google.maps.LatLng(45.777919, 3.082568),
             zoom: 17
         },
-    
+
     // Google maps marker for Atelier Preylude
         markerAtelier = new google.maps.Marker({
             position: new google.maps.LatLng(45.778444, 3.082543),
             title: 'Atelier Preylude',
-            icon: '../img/map-marker-themed.png'
+            icon: 'img/map-marker-fuschia.png'
         }),
-    
+
     // Owl Carousel options
         owlOptions = {
             items: 1,
@@ -32,13 +32,13 @@ $(document).ready(function () {
             animateIn: 'fadeIn',
             animateOut: 'fadeOut'
         };
-    
+
     // Load home page
     $.get('pages/' + initialPage + '.html', function (content) {
         container.html(content);
         loadedPages[initialPage] = content;
     });
-    
+
     // Create a link for each navbar item to load pages
     $('#navbar li').each(function (index) {
         var pageName = $(this).attr('data-link'),
@@ -48,13 +48,13 @@ $(document).ready(function () {
             container.animate({
                 opacity: 0
             }, 450, 'linear', function () {
-            
+
                 if (loadedPages.hasOwnProperty(link)) {
                     container.html(loadedPages[link]);
-                    
+
                     // Refresh dynamic content
                     dynamicHandler(pageName);
-                    
+
                     container.animate({
                         opacity: 1
                     }, 450, 'linear');
@@ -63,10 +63,10 @@ $(document).ready(function () {
                     $.get(link, function (content) {
                         container.html(content);
                         loadedPages[link] = content;
-                        
+
                         // Refresh dynamic content
                         dynamicHandler(pageName);
-                        
+
                         container.animate({
                             opacity: 1
                         }, 450, 'linear');
@@ -75,8 +75,8 @@ $(document).ready(function () {
             });
         });
     });
-    
-    
+
+
     // Handle google maps and carousel instanciation at page change
     function dynamicHandler (pageName) {
         if (pageName === 'infos') {
